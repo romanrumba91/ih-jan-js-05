a0e8f74bd6c14211def67e7b85adf5a07240b235
Roman <romandiazloz91@gmail.com>
2022-01-12 11:52:22 -0600

    agregado de prop

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ console.log(mexico.comida)
  //bracket notation
 
  console.log(mexico["comida"])
+
+ const olympicsRecords ={
+
+athletics100Men : "Justin Gatlin",
+athleticsLongJumpMen : "Mike Powel",
+swimming200Men: "Michael Phelps", 
+"swimming 200 Women": "Ariarne Titmus"
+
+ }
+
+ console.log(olympicsRecords.athletics100Men)
+ console.log(olympicsRecords["athletics100Men"])
+ console.log(olympicsRecords["swimming 200 Women"])
+
+ // agregar propiedades
+
+ const refresco = {
+    nombre: "Coca Cola",
+    color: " Oscuro",
+    precio: 12,
+    tamano: [600, 1000]
+
+ }
+
+ refresco.envase = "Plastico"
+
+ console.log(refresco)
+
+ refresco["competidor"] = "Pepsi"
+
+ console.log(refresco)
